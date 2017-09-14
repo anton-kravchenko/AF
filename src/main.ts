928e50085fb0e1404e05608fd46a80f6a3c7a672
antosha <t@gmai.com>
2017-09-14 22:57:09 +0300

    inline add styles loader

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+// + all ts should be bundled with web pack.
+// + scripts add to html automatically.
+// +- images included and put to the sources via webpack
+// All scripts should use OOP style
+// scss should be transpile into css
+// jquery should be included in common chunk(CommonChunk Plugin)
+
+
+// on the page we should show version of the script(version define by webpack)
+// version has to be shown only for development version of build
+
+
+// import $ = require("jquery");
+
+import ".././images/ang.png";
+import ".././images/wp.png";
+import ".././styles.scss";
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,43 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "styles.css",
+    disable: process.env.NODE_ENV === "development"
+});
 
 module.exports = {
-    entry: './main.ts',
+    entry: './src/main.ts',
     module: {
         rules: [
-        {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/
-        }
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.png$/,
+                use: [
+                    {
+                        loader :'url-loader',
+                        options: {
+                            limit: 10000
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [{
+                        loader: "css-loader"
+                    }, {
+                        loader: "sass-loader"
+                    }],
+                    fallback: "style-loader"
+                })
+            }
         ]
     },
     resolve: {
@@ -24,6 +52,8 @@ module.exports = {
             title: 'Hello Test',
             hash: true,
             template: './index.html'
-        })
-    ]
+        }),
+        extractSass
+    ],
+    devtool: "source-map"
 };
